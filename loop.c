97c493b84ecabda24d45875a2058e09f58d1dd1d
raining888 <42765754+raining888@users.noreply.github.com>
2022-04-11 21:23:54 +0800

    Update loop.c

--- loop.c
+++ loop.c
@@ -5,7 +5,11 @@ int main()
 {
   while(true)
   {
-
+    // 设置进程的nice值（优先级）
+    // nice的值介于-20~19直接，值越小分配的cpu占比越大
+    // renice -n [value] [pid]
+    // 绑定进程到cpu上
+    // taskset -cp [cpuid] [pid]
   }
   return 0;
 }
